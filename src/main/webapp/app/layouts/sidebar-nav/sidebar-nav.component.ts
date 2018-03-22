e275faf2a9a85232f6558f68eb27f6477b63cc78
Ngọc JR <ngocjr@Ngocs-MBP.net.fpt>
2018-03-22 20:56:19 +0700

    add Resume

--- src/main/webapp/app/layouts/sidebar-nav/sidebar-nav.component.ts
+++ src/main/webapp/app/layouts/sidebar-nav/sidebar-nav.component.ts
@@ -10,6 +10,7 @@ import { Component, OnInit, Input, Output, EventEmitter, OnChanges, SimpleChange
 export class SidebarNavComponent implements OnInit, DoCheck {
     @Input('choice') static choice: String = '';
     @Output('choiceChange') choiceChange: EventEmitter<String> = new EventEmitter<String>();
+    isVisible: Boolean = false;
     constructor() {
     }
 
@@ -20,8 +21,34 @@ export class SidebarNavComponent implements OnInit, DoCheck {
         this.choiceChange.emit(SidebarNavComponent.choice);
     }
 
+    send() {
+    }
+
+    onClickMessage() {
+        this.isVisible = !this.isVisible;
+        console.log(document);
+        const messageItem = document.getElementById('messageItem');
+        console.log(messageItem);
+        messageItem.style.height =  'auto';
+        // const mySidebar = document.getElementById('my-sidebar');
+        // console.log(mySidebar);
+        // mySidebar.style.width = '250px';
+    }
+
+    cancel() {
+        this.isVisible = false;
+        // console.log(document);
+        // const messageItem = document.getElementById('messageItem');
+        // console.log(messageItem);
+        // messageItem.style.height =  '61px';
+        // const mySidebar = document.getElementById('my-sidebar');
+        // console.log(mySidebar);
+        // // mySidebar.style.width = '60px';
+    }
+
     onClick() {
         SidebarNavComponent.choice = 'resume';
+        this.isVisible = false;
         this.choiceChange.emit(SidebarNavComponent.choice);
     }
 
